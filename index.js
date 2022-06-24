64ea6d58ac5c1204bcfa56d41173860342e70f29
jerryjohns <jerry.johns@student.moringaschool.com>
2022-06-24 12:11:02 +0300

    event js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// let addToDoBtn = document.getElementById('addToDo');
-// let toDoContainer =document.getElementById('toDoContainer');
-// let inputField = document.getElementById('inputField');
+let addToDoBtn = document.getElementById('addToDo');
+let toDoContainer =document.getElementById('toDoContainer');
+let inputField = document.getElementById('inputField');
 
-// addToDoBtn.addEventListener('click', function(){
-//     var paragraph = document.createElement('p')
-//     paragraph.innerText = inputField.value;
-//      toDoContainer.appendChild(paragraph);
-//     inputField.value = "";
+addToDoBtn.addEventListener('click', function(){
+    var paragraph = document.createElement('p')
+    paragraph.innerText = inputField.value;
+     toDoContainer.appendChild(paragraph);
+    inputField.value = "";
 //     paragraph.addEventListener('click',function(){
 //         paragraph.style.textDecoration = "line-through";
 //     })
